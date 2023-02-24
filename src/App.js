18382a864c822a48b5073adf776147f9a0f41817
jihacshnu <jishnuchowdhury30@gmail.com>
2023-02-24 19:57:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+// import About from './components/About';
+
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [Mode, setMode] = useState('light');
+  
+  const [alert, setAlert] = useState(null);
+
+const showAlert=(message,type)=>{
+  setAlert({
+    msg:message,
+    typ:type
+  })
+  setTimeout(() => {
+    setAlert(null)
+  }, 1500);
 }
 
+  const remClass=()=>{
+    document.body.classList.remove('bg-light')
+    document.body.classList.remove('bg-dark')
+    document.body.classList.remove('bg-danger')
+    document.body.classList.remove('bg-primary')
+    document.body.classList.remove('bg-success')
+    document.body.classList.remove('bg-warning')
+  }
+
+  const toggleMode=(cls)=>{
+    remClass();
+    document.body.classList.add('bg-'+cls)
+    if(Mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor='#042743'
+      showAlert('Dark mode has been enabled','success')
+      document.title='TextUI-Dark Mode'
+    }
+    else {
+      setMode('light')
+      document.body.style.backgroundColor='white'
+      showAlert('Light mode has been enabled','success')
+     
+      
+        document.title='TextUI-Light Mode'
+      
+    }
+   
+  }
+  return (
+    <>
+
+  {/* <Router> */}
+        <Navbar title="TextUI" mode={Mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+        <Textform showAlert={showAlert} heading="Enter your text" mode={Mode} />
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}> </Route> */}
+            {/* <Link className="nav-link" to={'/about'}>About</Link> */}
+            {/* <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter your text" mode={Mode} />}></Route> */}
+          {/* </Routes> */}
+        </div>
+      {/* </Router> */}
+</>
+  );
+}
+ 
 export default App;
---
+++ src/components/Textform.js
@@ -0,0 +1,105 @@
+import React,{useState} from 'react'
+
+
+export default function Textform(props) {
+  const handelUpClick = ()=>{
+  
+    let upText=text.toUpperCase()
+    setText(upText)
+    if(text.length===0){
+      props.showAlert("Please enter text!",'warning')
+    }
+    else{
+      props.showAlert("Converted to uppercase!",'success')
+    }
+  }
+  
+  const handelLoClick = ()=>{
+    let loText=text.toLowerCase()
+    setText(loText)
+    
+    if(text.length===0){
+      props.showAlert("Please enter text!",'warning')
+    }
+    else{
+      props.showAlert("Converted to lowercase!",'success')
+    }
+  }
+  
+  const handelClear = ()=>{
+    let clText="";
+    setText(clText)
+    
+    if(text.length===0){
+      props.showAlert("Please enter text!",'warning')
+    }
+    else{
+      props.showAlert("Clear text!",'warning')
+    }
+
+  }
+  
+
+
+const handelCopy=()=>{
+  var copyText=document.getElementById("textBox")
+  copyText.select()
+  navigator.clipboard.writeText(copyText.value)
+  
+  if(text.length===0){
+    props.showAlert("Please enter text!",'warning')
+  }
+  else{
+    props.showAlert("Text copied!",'success')
+  }
+}
+
+const handelExtraSpace=()=>{
+  let spaceText=text.split(/[ ]+/)
+  setText(spaceText.join(" "))
+  
+  if(text.length===0){
+    props.showAlert("Please enter text!",'warning')
+  }
+  else{
+    props.showAlert("Extra space has been removed!",'success')
+  }
+}
+  
+  const handelonChange = (event)=>{
+   
+    setText(event.target.value)
+  }
+ 
+  const[text,setText]= useState("");
+  // text="new text"; wrong Way 
+  // setText("new text"); correct way
+  
+  return (
+    <>
+ 
+  <div className='container' style={{color:props.mode==='dark'?'white':'black'}}> 
+  <h1>{props.heading}</h1>
+  <div className="mb-3">
+  
+     <textarea className="form-control" value={text} id="textBox" rows="8" onChange={handelonChange} style={{backgroundColor:props.mode==='dark'?'#181818':'white' ,color:props.mode==='dark'?'white':'black'}}></textarea>
+     
+  </div >
+  <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} mx-1`} onClick={handelUpClick}>Convert to uppercase</button>
+  <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} mx-1`} onClick={handelLoClick}>Convert to lowercase</button>
+  <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} mx-1`} onClick={handelClear}> ClearText</button>
+  <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} mx-1`} onClick={handelCopy}> Copy Text</button>
+  <button className={`btn btn-${props.mode==='dark'?'dark':'primary'} mx-1`} onClick={handelExtraSpace}>Remove extra space </button>
+  
+</div>
+<div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
+  <h2>Your text summary</h2>
+  <p>{text.split(" ").length} words and {text.length} characters</p>
+  <p>{0.008*text.split(" ").length} Minutes to read </p>
+  <h2>Perview</h2>
+  <p>{text.length>0?text:"Enter something to preview"}</p>
+</div>
+
+</>   
+  )
+}
